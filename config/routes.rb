66f092755407d1a36beb09ebc486d2b4bd7effde
Tyler Epstein <tylerepstein27@gmail.com>
2021-03-11 23:36:27 -0500

    adds authentication and authorization

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,8 @@ Rails.application.routes.draw do
     post "/movies" => "movies#create"
     patch "/movies/:id" => "movies#update"
     delete "/movies/:id" => "movies#destroy"
+    #--------------------------------------
+    post "/users" => "users#create"
+    post "/sessions" => "sessions#create"
   end
 end

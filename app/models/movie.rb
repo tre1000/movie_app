de10b9f6fa97be1e4caefce3acbe11dd46c94efd
Tyler Epstein <tylerepstein27@gmail.com>
2021-03-14 23:20:13 -0400

    adds has-many-through association to movies and genre

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ApplicationRecord
   has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
   # validates :title, presence: true, length: { maximum: 200 }
   # longest movie title ever is only 196 characters, so this should be good.
   # validates :year, presence: true, numericality: { only_integer: true }, length: { is: 4 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_12_041840) do
+ActiveRecord::Schema.define(version: 2021_03_15_030717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,19 @@ ActiveRecord::Schema.define(version: 2021_03_12_041840) do
     t.string "known_for"
     t.string "gender"
     t.string "age"
+    t.integer "movie_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "movie_genres", force: :cascade do |t|
+    t.integer "genre_id"
     t.integer "movie_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false

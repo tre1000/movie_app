d2d1a54f0620c2bd6f1513927f10e0b20d0c5908
Tyler Epstein <tylerepstein27@gmail.com>
2021-04-11 22:23:13 -0400

    removes some migrations that might have been messing with heroku

--- db/migrate/20210308232953_add_movie_to_actors.rb
+++
@@ -1,5 +0,0 @@
-class AddMovieToActors < ActiveRecord::Migration[6.1]
-  def change
-    add_column :actors, :movie_id, :integer
-  end
-end
--- db/migrate/20210308235455_drop_actors.rb
+++
@@ -1,5 +0,0 @@
-class DropActors < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :actors
-  end
-end
